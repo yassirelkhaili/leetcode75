2fa0250568cb1a63e6b677f3159d518edfb7933e
Yassir <elkhailiyassir@gmail.com>
2023-10-04 10:17:17 +0200

    pushed

--- FindDiffrence.ts
+++ FindDiffrence.ts
@@ -17,14 +17,31 @@
 
 //using javascript sets data structure
 
+// function findDifference(nums1: number[], nums2: number[], answer: number[][] = [[], []]): number[][] {
+//     const numSet1 = new Set(nums1)
+//     const numSet2 = new Set(nums2)
+//     for (const num of nums1)
+//         !(numSet2.has(num) || answer[0].includes(num)) && answer[0].push(num)
+//     for (const num of nums2)
+//         !(numSet1.has(num) || answer[1].includes(num)) && answer[1].push(num)
+//     return answer
+// };
+
+//using a hashmap (frequency map) for best performance O(n) where n is the length of the longest array
+
 function findDifference(nums1: number[], nums2: number[], answer: number[][] = [[], []]): number[][] {
-    const numSet1 = new Set(nums1)
-    const numSet2 = new Set(nums2)
+    const frequencyMap: Map<number, number> = new Map();
     for (const num of nums1)
-        !(numSet2.has(num) || answer[0].includes(num)) && answer[0].push(num)
+        frequencyMap.set(num, (frequencyMap.get(num) || 0) + 1)
     for (const num of nums2)
-        !(numSet1.has(num) || answer[1].includes(num)) && answer[1].push(num)
+        frequencyMap.set(num, (frequencyMap.get(num) || 0) - 1)
+    for (const [num, frequency] of frequencyMap)
+    {
+        if (frequency > 0 && !answer[0].includes(num))
+            answer[0].push(num)
+        else if (frequency < 0 && !answer[1].includes(num))
+            answer[1].push(num)
+    }
     return answer
 };
-
 console.log(findDifference([1,2,3], [2,4,6]))
