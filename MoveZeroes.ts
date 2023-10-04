d9f283ab6efcf45cbb34e40d7b59ed4b16c1debd
Yassir <elkhailiyassir@gmail.com>
2023-10-04 10:23:28 +0200

    pushed

--- MoveZeroes.ts
+++ MoveZeroes.ts
@@ -25,7 +25,6 @@ let nums: Array<number> = [0,1,0,3,12];
 
 function moveZeroes(nums: number[]): void {
     let moveToFirst = 0;
-
     for (let i = 0; i < nums.length; i++) {
         if (nums[i] !== 0) {
             [nums[i], nums[moveToFirst]] = [nums[moveToFirst], nums[i]];
--- gcdOfStrings.ts
+++ gcdOfStrings.ts
@@ -1,9 +1,28 @@
-let str1 = "ABCABC";
-let str2 = "ABC";
+//Eucledian algorithm iterative
+
+const gcdi = (a: number, b: number) : number => {
+    let result: number = a % b;
+    while (result != 0)
+    {
+        a = b;
+        b = result;
+        result = a % b;
+    }
+    return b;
+}
+
+//Eucledian algorithm recursion
+
+const gcdr = (a: number, b: number) : number => (b === 0 ? a : gcdr(b, a % b));
+
+//Main function
 
 function gcdOfStrings(str1: string, str2: string): string {
-let result : Array<string> = [];
-
-return result.join('');
-};
-console.log(gcdOfStrings(str1, str2));
+    const strlen = str1.length;
+    const strlen2 = str2.length;
+    const gcdlen = gcdi(strlen, strlen2);
+    const gcd = str2.substring(0, gcdlen);
+    if (str1 + str2 == str2 + str1 && strlen % gcdlen === 0 && strlen2 % gcdlen === 0)
+        return gcd;
+    return "";
+}
