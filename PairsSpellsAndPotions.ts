4442a673fe9609daed1dd0728f87f6ae40258eb8
Yassir <elkhailiyassir@gmail.com>
2023-10-05 12:58:14 +0200

    pushed

--- PairsSpellsAndPotions.ts
+++ PairsSpellsAndPotions.ts
@@ -1,11 +1,11 @@
 //first solution too slow buddy
 
-function successfulPairs(spells: number[], potions: number[], success: number, index: number = 0, result: Array<number> = new Array(spells.length).fill(0)): number[] {
-    while (index < spells.length)
-    {
-        for (const num of potions)
-            num * spells[index] >= success && (result[index] += 1)
-        index++;
-    }
-    return result
-};
+// function successfulPairs(spells: number[], potions: number[], success: number, index: number = 0, result: Array<number> = new Array(spells.length).fill(0)): number[] {
+//     while (index < spells.length)
+//     {
+//         for (const num of potions)
+//             num * spells[index] >= success && (result[index] += 1)
+//         index++;
+//     }
+//     return result
+// };
